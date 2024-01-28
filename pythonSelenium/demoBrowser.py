2c5d626cebdc29be56e8347debce0d34755efc70
sucheta-mandar <suchetamandar@gmail.com>
2024-01-28 22:31:28 +0530

    URl details

--- pythonSelenium/demoBrowser.py
+++ pythonSelenium/demoBrowser.py
@@ -1,19 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
 with open('setting.txt', 'r') as file:
     setting = file.readlines()
-    videoLink = (setting[0].split(": "))[1]
+    browser = (setting[0].split(": "))[1]
+    URL = (setting[1].split(": "))[1]
+
 
 service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
-driver.get("https://www.youtube.com/"+videoLink)
+driver.get(URL)
 driver.maximize_window()
-
-driver.find_element(By.XPATH,"//div[@class='ytp-left-controls']/button[@class='ytp-play-button ytp-button']").click()
-maxtime = driver.find_element(By.XPATH,"//div[@class='ytp-left-controls']/div[@class='ytp-time-display notranslate']/span[2]/span[@class='ytp-time-duration']").text
-print(maxtime)
-time = maxtime.split(":")
-videoTime = (int(time[0])*60) + int(time[1])
-print(videoTime)
+print(driver.title)
